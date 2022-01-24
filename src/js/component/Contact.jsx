852b5e803516df3468c54390e939e4c0fb3774e8
BernatLl <blloret@hotmail.com>
2022-01-24 16:43:42 +0000

    last update 240122

---
+++ src/js/component/Contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../styles/Contact.css";
+
+const Contact = () => {
+	return (
+		<div className="container-form">
+			<form id="myform">
+				<div className="mb-3">
+					<label
+						for="exampleFormControlInput1"
+						className="form-label">
+						Email address
+					</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleFormControlInput1"
+						placeholder="name@example.com"></input>
+				</div>
+				<div class="mb-3">
+					<label
+						for="exampleFormControlTextarea1"
+						className="form-label">
+						Example textarea
+					</label>
+					<textarea
+						className="form-control"
+						id="exampleFormControlTextarea1"
+						rows="3"></textarea>
+				</div>
+			</form>
+		</div>
+	);
+};
+export default Contact;
--- src/js/view/home.jsx
+++ src/js/view/home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Footer from "../component/Footer.jsx";
+import Contact from "../component/Contact.jsx";
 
 const Home = () => {
-	return <Footer />;
+	return (
+		<>
+			<Contact />
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
