8dafcb7744f0de885bb8f62f78e4a8499e61aca1
Orealba <orealbas@gmail.com>
2022-01-24 15:41:26 +0000

    header

---
+++ src/js/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/Header.css";
+
+const Header = () => {
+	return (
+		<div className="titulo">
+			<h1>Futuros programadores</h1>
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/js/view/home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../component/Header.jsx";
+
+const Home = () => {
+	return <Header />;
+};
+
+export default Home;
