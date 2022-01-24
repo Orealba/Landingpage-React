da0eaf287a7ce97f829062b758b78c01373d6100
BernatLl <blloret@hotmail.com>
2022-01-24 15:55:55 +0000

    er

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -4,17 +4,7 @@ import "../../styles/Footer.css";
 const Footer = () => {
 	return (
 		<div className="container-footer">
-			<div>
-				<p>Contenido</p>
-			</div>
-			<div>
-				<p>Redes Sociales</p>
-			</div>
-			<div>
-				<p>
-					Contenido Legal
-				</p>
-			</div>
+			<p>Contenido</p>
 		</div>
 	);
 };
