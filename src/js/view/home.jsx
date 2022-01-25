a628e905714778f354617d3265d09b11ae2d2347
BernatLl <blloret@hotmail.com>
2022-01-25 10:45:02 +0000

    er

--- src/js/view/home.jsx
+++ src/js/view/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../component/Footer.jsx";
 import Contact from "../component/Contact.jsx";
+import Seccion2 from "../component/Seccion2.jsx";
 
 const Home = () => {
 	return (
 		<>
+			<Seccion2 />
 			<Contact />
 			<Footer />
 		</>
