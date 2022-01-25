590d4ca2c737286b9c5083404dd58e6442d16c68
BernatLl <blloret@hotmail.com>
2022-01-25 13:29:06 +0000

    update

--- src/js/component/Seccion2.jsx
+++ src/js/component/Seccion2.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "../../styles/Seccion2.css";
+import imagen1 from "../../img/imagen1.jpg";
 
 const Seccion2 = () => {
 	return (
 		<>
-			<div className="card mb-3 m-5">
+			<div className="card m-5">
 				<img
-					src="https://c.pxhere.com/images/47/2a/e7c67294be044b9374d0fc78f096-1455461.jpg!d"
+					src={imagen1}
 					width="auto"
 					height="500"
 					className="card-img-top"
 					alt="Reunion"></img>
 				<div className="card-body">
-					<h5 className="card-title">A donde vamos ....</h5>
-					<p className="card-text">
-						This is a wider card with supporting text below as a
-						natural lead-in to additional content. This content is a
-						little bit longer.
-					</p>
+					<h5 className="card-title">El Equipo...</h5>
+					<p className="card-text"></p>
 					<p className="card-text">
 						<small className="text-muted">
 							Last updated 3 mins ago
