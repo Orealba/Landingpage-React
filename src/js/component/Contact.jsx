218d667c797421ab7401aa4790a77f9e14c22873
BernatLl <blloret@hotmail.com>
2022-01-25 14:03:59 +0000

    update

--- src/js/component/Contact.jsx
+++ src/js/component/Contact.jsx
@@ -30,6 +30,17 @@ const Contact = () => {
 						rows="3"
 						required></textarea>
 				</div>
+				<div class="form-check mb-3">
+					<input
+						class="form-check-input"
+						type="radio"
+						name="flexRadioDefault"
+						id="flexRadioDefault1"
+						required></input>
+					<label class="form-check-label" for="flexRadioDefault1">
+						<a href="#">Acepta la politica de privacidad</a>
+					</label>
+				</div>
 				<div>
 					<button type="submit" className="btn btn-primary">
 						Contáctanos
