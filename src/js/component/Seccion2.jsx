00e4d76d11d42947d11d795200c4cbaf9fd59536
BernatLl <blloret@hotmail.com>
2022-01-25 11:31:05 +0000

    er

--- src/js/component/Seccion2.jsx
+++ src/js/component/Seccion2.jsx
@@ -2,25 +2,31 @@ import React from "react";
 import "../../styles/Seccion2.css";
 
 const Seccion2 = () => {
-    return (
-        <> <div className="card mb-3">
-            <img src="..." className="card-img-top" alt="..."></img>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-        </div>
-            <div className="card">
-                <div class="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-                <img src="..." className="card-img-bottom" alt="..."></img>
-            </div>
-        </>
-
-    );
-}
+	return (
+		<>
+			{" "}
+			<div className="card mb-3 m-5">
+				<img
+					src="https://c.pxhere.com/images/47/2a/e7c67294be044b9374d0fc78f096-1455461.jpg!d"
+					width="auto"
+					height="300"
+					className="card-img-top"
+					alt="Reunion"></img>
+				<div className="card-body">
+					<h5 className="card-title">A donde vamos ....</h5>
+					<p className="card-text">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</p>
+					<p className="card-text">
+						<small className="text-muted">
+							Last updated 3 mins ago
+						</small>
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
 export default Seccion2;
